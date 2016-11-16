b9e227b1d31288edee04e35272e447c54521cd16
zoehawkesford <zoe.hawkesford.14@ucl.ac.uk>
2016-11-16 16:35:58 +0000

    I added stuff guuuuys!

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
+  get 'users/new'
+
   resources :events
 
   root 'welcome#index'
+
+  get  '/signup',  to: 'users#new'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
