3b532d9c7827b4f232fb0c5e2b43e18f16d5fcb8
zoehawkesford <zoe.hawkesford.14@ucl.ac.uk>
2016-11-17 22:42:12 +0000

    Hopefully a working sign up page

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,9 @@
 Rails.application.routes.draw do
-  get 'users/new'
-
-  resources :events do
-    resources :comments
-  end
-
-  root 'welcome#index'
-
+  root 'static_pages#home'
+  get  '/help',    to: 'static_pages#help'
+  get  '/about',   to: 'static_pages#about'
+  get  '/contact', to: 'static_pages#contact'
   get  '/signup',  to: 'users#new'
   post '/signup',  to: 'users#create'
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
   resources :users
 end
