0d48ebf4b07e52f679bbd94acbb4844aca41ce1b
zoehawkesford <zoe.hawkesford.14@ucl.ac.uk>
2016-11-18 18:36:16 +0000

    log in stuff - seem to have lost sign up?

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+
   get 'users/new'
 
   resources :events do
@@ -6,9 +8,16 @@ Rails.application.routes.draw do
   end
 
   root 'welcome#index'
+  #root   'static_pages#home'
+  get    '/help',    to: 'static_pages#help'
+  # get    '/about',   to: 'static_pages#about'
+  # get    '/contact', to: 'static_pages#contact'
 
   get  '/signup',  to: 'users#new'
   post '/signup',  to: 'users#create'
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
