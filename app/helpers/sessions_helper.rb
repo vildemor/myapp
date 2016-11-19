bd38e9b82fbdb93b74ca6538545f8f9c8ca0c506
zoehawkesford <zoe.hawkesford.14@ucl.ac.uk>
2016-11-19 12:12:07 +0000

    AH LOG IN LOG OUT SORTED

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -13,10 +13,10 @@ module SessionsHelper
   # Returns true if the user is logged in, false otherwise.
   def logged_in?
     !current_user.nil?
+  end
     # Logs out the current user.
   def log_out
     session.delete(:user_id)
     @current_user = nil
-    end
   end
 end
