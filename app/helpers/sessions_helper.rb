d7112dbd5821c1e8e803475d1dd2a02882169272
zoehawkesford <zoe.hawkesford.14@ucl.ac.uk>
2016-11-19 11:48:23 +0000

    log in stuff - seem to have lost 'account' dropdown menu when you log in? But now you can log out.

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -13,5 +13,10 @@ module SessionsHelper
   # Returns true if the user is logged in, false otherwise.
   def logged_in?
     !current_user.nil?
+    # Logs out the current user.
+  def log_out
+    session.delete(:user_id)
   end
+    @current_user = nil
+    end
 end
